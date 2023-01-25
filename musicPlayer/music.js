b66681bada189a4c5f5b2e0be445ad8a9553feeb
zaid83 <34624921+zaid83@users.noreply.github.com>
2023-01-25 14:31:57 +0100

    Add files via upload
    First version

---
+++ musicPlayer/music.js
@@ -0,0 +1,46 @@
+// Récuperer  les élémeents du DOM par l'id
+
+let progress = document.getElementById("progress");
+let song = document.getElementById("song");
+let ctrlIcon = document.getElementById("ctrlIcon");
+
+// Durée de la musique = barre de progression
+
+song.onloadedmetadata = function () {
+    progress.max = song.duration;
+    progress.value = song.currentTime;
+}
+
+// Fonction Play/Pause avec changement d'icones
+
+function playPause() {
+    if (ctrlIcon.classList.contains('fa-pause')) {
+        song.pause();
+        ctrlIcon.classList.add('fa-play');
+        ctrlIcon.classList.remove('fa-pause');
+
+    }
+    else {
+        song.play();
+        ctrlIcon.classList.remove('fa-play');
+        ctrlIcon.classList.add('fa-pause');
+    }
+}
+
+// Synchroniser la barre de progression avec la musique ttes les 500 milisecondes
+
+if (song.play()) {
+    setInterval(() => {
+        progress.value = song.currentTime;
+    }, 500)
+}
+
+// Changer le temps de la musique lorsque l'on clique sur la barre de progression
+
+progress.onchange = function (){
+    song.play();
+    song.currentTime = progress.value;
+    ctrlIcon.classList.remove('fa-play');
+    ctrlIcon.classList.add('fa-pause');
+
+}
