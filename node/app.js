e7d151ba3f1bde0e149fe7fde220ddafd8265bbb
zaid83 <zaidchakir@yahoo.fr>
2023-03-21 19:18:35 +0100

    pokemon

--- node/app.js
+++ node/app.js
@@ -1,11 +1,21 @@
+const bodyParser = require("body-parser");
 const express = require("express");
+const morgan = require("morgan");
+const favicon = require("serve-favicon")
 const app = express();
 const port = 3000;
 const { success } = require('./helper.js')
+const { getUniqueId } = require('./helper.js')
 let pokemons = require('./mock-pokemon')
+
+app.use(favicon(__dirname + '/img/favicon.ico'))
+    .use(morgan('dev'))
+    .use(bodyParser.json())
+
 
 app.get('/api/pokemons/', (req, res) => {
     const message = "La liste des Pokemons a bien été envoyé"
+
     res.json(success(message, pokemons))
 
 });
@@ -15,4 +25,14 @@ app.get('/api/pokemons/:id', (req, res) => {
     const message = "UN pokémon a été trouvé";
     res.json(success(message, pokemon));
 })
+
+app.post('/api/pokemons', (req, res) => {
+    const id = getUniqueId(pokemons);
+    const pokemonCreated = { ...req.body, ...{ id: id, created: new Date() } }
+    pokemons.push(pokemonCreated)
+    const message = `Le pokémon ${pokemonCreated.name} a bien été crée.`
+
+    res.json(success(message, pokemonCreated))
+})
+
 app.listen(port, () => console.log(`Notre appli demarre sur http://localhost:${port}`));
