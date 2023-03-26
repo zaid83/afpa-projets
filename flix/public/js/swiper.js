181a5eb8ad06d624eade2181dbcef8131c04c629
zaid83 <zaidchakir@yahoo.fr>
2023-03-27 01:05:24 +0200

    version 0.2

---
+++ flix/public/js/swiper.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    loop: true,
+
+    spaceBetween: 30,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
